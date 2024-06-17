10c68c1759bb0aedcc69a2dde6780a73068a7c4d
Austin Karren <austin.t.karren@gmail.com>
2024-06-16 18:24:54 -0600

    about page content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -53,8 +53,7 @@ function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
 
 export const metadata: Metadata = {
   title: 'About',
-  description:
-    'I’m Austin Karren. I live in New York City, where I design the future.',
+  description: 'Hi, I’m Austin Karren.',
 }
 
 export default function About() {
@@ -73,35 +72,35 @@ export default function About() {
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            I’m Austin Karren. I live in New York City, where I design the
-            future.
+            Hi, I’m Austin Karren.
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
-              I’ve loved making things for as long as I can remember, and wrote
-              my first program when I was 6 years old, just two weeks after my
-              mom brought home the brand new Macintosh LC 550 that I taught
-              myself to type on.
+              I’ve loved making things for as long as I can remember. When I was
+              young, I loved building things with Legos. Whether I was following
+              instructions or creating an entirely new design myself, I enjoyed
+              creating things.
             </p>
             <p>
-              The only thing I loved more than computers as a kid was space.
-              When I was 8, I climbed the 40-foot oak tree at the back of our
-              yard while wearing my older sister’s motorcycle helmet, counted
-              down from three, and jumped — hoping the tree was tall enough that
-              with just a bit of momentum I’d be able to get to orbit.
+              My first experience programming things was in a video game called
+              Little Big Planet. What makes Little Big Planet interesting is
+              that the developers made an in-game creative mode that is powerful
+              enough to recreate every level in the game. I learned how to
+              design and program exciting things like auto-defense turrets,
+              spaceships, and pilotable mech suits.
             </p>
             <p>
-              I spent the next few summers indoors working on a rocket design,
-              while I recovered from the multiple surgeries it took to fix my
-              badly broken legs. It took nine iterations, but when I was 15 I
-              sent my dad’s Blackberry into orbit and was able to transmit a
-              photo back down to our family computer from space.
+              I quickly learned that I enjoy working at the intersection of good
+              design and programming. In college, I learned about computer
+              science concepts such as object-oriented programming, design
+              patterns, and functional programming, but coding truly clicked for
+              me when I began working on web development.
             </p>
             <p>
-              Today, I’m the founder of Planetaria, where we’re working on
-              civilian space suits and manned shuttle kits you can assemble at
-              home so that the next generation of kids really <em>can</em> make
-              it to orbit — from the comfort of their own backyards.
+              Today, I am the technical co-founder of Event Penguin, where we
+              are building the best event registration platform for small to
+              medium-sized events. I am dedicated to solving problems with apps
+              that are a <em>delight</em> to use.
             </p>
           </div>
         </div>
