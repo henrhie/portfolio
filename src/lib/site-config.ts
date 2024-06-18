dfa3ae42942ee478bd812dfd14438ba71159486c
Austin Karren <austin.t.karren@gmail.com>
2024-06-17 18:09:14 -0600

    updated site description

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -29,7 +29,7 @@ export type SiteConfig = z.infer<typeof SiteSchema>
 export const siteConfig = {
   title: 'Austin Karren',
   author: 'Austin Karren',
-  description: `I’m Austin, a software engineer and entrepreneur based in Vineyard Utah. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.`,
+  description: `I’m Austin, a software engineer and entrepreneur based in Utah. I’m the technical co-founder of Event Penguin, where we are making the best registration tool for small to medium sized events. Looking for work.`,
   email: 'ausitn@karren.com',
   calLink: 'austin-karren',
   calUrl: 'https://cal.com/austin-karren',
